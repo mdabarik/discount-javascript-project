229cb24f6ec5254dba8a5682ffd3ec6f342cf662
mdabarik <mdabarik19@gmail.com>
2023-08-17 01:46:00 +0600

    project done

--- js/pay.js
+++ js/pay.js
@@ -1,2 +1,28 @@
 
-const oldPrice = getValueById('old-price');
+const coupons = {
+    LEVELUP25: "25%",
+    BIGSALE: "20%",
+    BARIKID: "50%",
+    HAPPYNEWYEAR: "30%"
+}
+
+document.getElementById('btn-apply').addEventListener('click', function(e) {
+    const oldPrice = getValueById('old-price');
+    const getCoupon = document.getElementById('input-field').value.trim();
+    document.getElementById('input-field').value = "";
+    if (!(getCoupon in coupons)) {
+        document.getElementById('coupon-info').innerHTML = `<p class='text-red-300'> Invalid token. Please enter valid token </p>`
+    } else {
+        if (getValueById('old-price') > getValueById('new-price')) {
+            document.getElementById('coupon-info').innerHTML = `You saved: $ <span id="discount-for-you">${discount}</span> <p>(Already used coupon: ${getCoupon})</p>`;
+            return;
+        }
+        const percent = parseFloat(coupons[getCoupon]) / 100;
+        const discount = (oldPrice * percent).toFixed(2);
+        const newPrice = (oldPrice - discount).toFixed(2);
+        setTextById('new-price', newPrice);
+        document.getElementById('coupon-info').innerHTML = `You saved: $ <span id="discount-for-you">${discount}</span> <p>(coupon used: ${getCoupon})</p>`
+        document.getElementById('off').innerText = parseFloat(coupons[getCoupon]);
+    }
+})
+
--- js/util.js
+++ js/util.js
@@ -1,8 +1,12 @@
 
-// get value from an element by id
 function getValueById(id) {
     const element = document.getElementById('old-price');
     const elementValueString = element.innerText;
     const elementValue = parseFloat(elementValueString);
     return elementValue;
+}
+
+function setTextById(id, text) {
+    const element = document.getElementById(id);
+    element.innerText = text;
 }
